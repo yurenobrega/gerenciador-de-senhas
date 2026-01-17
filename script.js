6a54309b9a435a29d148bac62504e37b479213a7
Yure <yurenobrega221b@gmail.com>
2026-01-16 21:45:42 -0300

    JavaScript inicial

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let password1 = document.getElementById("password-btn-1")
+let password2 = document.getElementById("password-btn-2")
+
 const characters = [
   "a","b","c","d","e","f","g","h","i","j","k","l","m",
   "n","o","p","q","r","s","t","u","v","w","x","y","z",
@@ -5,3 +8,19 @@ const characters = [
   "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
   "!","@","#","$","%","^","&","*","_","-","+","=","?"
 ];
+
+function generatePasswords() {
+    if (password1.textContent != "" && password2.textContent != ""){
+        password1.textContent = ""
+        password2.textContent = ""
+    }
+    for(let i = 0; i < 15; i++){
+        password1.textContent += characters[getRandomChar()]
+        password2.textContent += characters[getRandomChar()]
+    }
+}
+
+function getRandomChar(){
+    return Math.floor(Math.random() * characters.length)
+}
+
